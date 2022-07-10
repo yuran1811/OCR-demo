73e10564bee910b15843b8a65b401ea58a584eaf
yuran1811 <trieuvanbd123@gmail.com>
2022-07-10 16:32:34 +0700

    fix bugs

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,25 +6,28 @@ const Home = () => {
 	return (
 		<div className='w-full p-8'>
 			<div className='w-full sm:w-[70%] p-8 mx-auto my-6 font-bold text-[5.5rem] text-center text-indigo-800'>
-				Welcome to Text recognition
+				Welcome to OCR Demo
 			</div>
 			<div className='w-full sm:w-[70%] p-8 mx-auto my-6'>
-				<div className='font-semibold text-[3.6rem] text-center'>Method</div>
-				<ul className='flex items-center justify-center'>
-					{methods.map(({ name, slug }) => (
-						<li key={slug} className='m-4'>
-							<Link className='p-6 text-[2.4rem] hover:underline underline-offset-8' to={`/${slug}`}>
-								{name}
-							</Link>
-						</li>
-					))}
-				</ul>
+				<div className='pb-8'>
+					<div className='font-semibold text-[3.6rem] text-center'>Method</div>
+					<ul className='flex items-center justify-center'>
+						{methods.map(({ name, slug }) => (
+							<li key={slug} className='m-4'>
+								<Link className='p-6 text-[2.4rem] hover:underline underline-offset-8' to={`/${slug}`}>
+									{name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
 
 				<div>
-					<div>More info:</div>
+					<div className='font-semibold text-[3.6rem] text-center'>More info:</div>
 					<ul>
-						<li>
+						<li className='text-center m-4'>
 							<a
+								className='p-6 text-[2.4rem] hover:underline underline-offset-8'
 								href='https://www.smashingmagazine.com/2021/06/image-text-conversion-react-tesseract-js-ocr/'
 								target='_blank'
 								rel='noopener noreferrer'
@@ -35,6 +38,18 @@ const Home = () => {
 					</ul>
 				</div>
 			</div>
+
+			<footer className='relative w-full text-center px-[1rem] pt-[0.7rem] pb-[2rem]'>
+				Made by{' '}
+				<a
+					className='underline underline-offset-8'
+					href='https://github.com/yuran1811/ocr-demo'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					yuran1811
+				</a>
+			</footer>
 		</div>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,1 @@
-module.exports={content:["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],theme:{extend:{}},plugins:[]};
+module.exports={content:["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],theme:{extend:{}},plugins:[]}
